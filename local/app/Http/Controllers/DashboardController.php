00f71d4ab0081191382e06118a860f4fe7ae1a2b
Pop101035 <pop101035@hotmail.com>
2016-09-11 20:30:49 +0700

    create command controller

--- local/app/Http/Controllers/DashboardController.php
+++ local/app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;  //date-times
 use View;
+use App\Oncall;
 
 class DashboardController extends Controller
 {
@@ -12,7 +13,7 @@ class DashboardController extends Controller
     public function index()
     {
       $current = Carbon::now();
-
+      $dash_data = Oncall::all();
 
         return View::make('dashboard')->with('current', $current);
         //return view('dashboard',);
